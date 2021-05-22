3b3557fc8b0a8c6313effeca041b83d3a74c49d8
robee103 <robee103@rediffmail.com>
2021-05-22 10:28:33 +0530

    Code setup

---
+++ src/test/java/com/robee/shorturl/ShorturlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robee.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
